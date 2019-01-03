222acaf8cb9a7a21a3b628949766dba470376501
john2796 <jbmiranda22796@gmail.com>
2019-01-02 16:56:52 -0800

    finished

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
-import { setCurrentUser, logoutUser } from "./actions/authActions";
+import { setCurrentUser, logoutUser } from "./store/actions/authActions";
 import { Provider } from "react-redux";
 import store from "./store/store";
 import Navbar from "./components/layout/Navbar";
