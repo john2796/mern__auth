fa5c48f6fbfeba111f91ee39dceb55ed791eee6e
john2796 <jbmiranda22796@gmail.com>
2019-01-02 16:08:57 -0800

    adding redux action for auth users

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,11 +1,10 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
+import rootReducer from "../store/reducers/index";
 
-const initialState = {};
 const middleware = [thunk];
 const store = createStore(
-  () => [],
-  initialState,
+  rootReducer,
   compose(
     applyMiddleware(...middleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
